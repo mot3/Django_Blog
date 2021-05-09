2ecce8b2515451562609347cadf6061b91c77ee5
mot3 <hopdrmot3@gmail.com>
2021-05-09 13:05:46 +0430

    first commit

---
+++ blog/tests.py
@@ -0,0 +1,45 @@
+import django
+from django.contrib.auth import get_user_model
+from django.db.models.fields.mixins import NOT_PROVIDED
+from django.http import response
+from django.test import TestCase
+from django.urls import reverse
+from django.views.generic.dates import timezone_today
+
+from .models import Post
+
+class BlogTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username = 'testuser',
+            email = 'test@gmail.com',
+            password = 'secret',
+        )
+
+        self.post = Post.objects.create(
+            title = 'A test title',
+            body = 'Nice body content',
+            author = self.user,
+        )
+
+    def test_string_representation(self):
+        post = Post(title='A sample title')
+        self.assertEqual(str(post), post.title)
+
+    def test_post_content(self):
+        self.assertEqual(f'{self.post.title}', 'A test title')
+        self.assertEqual(f'{self.post.author}', 'testuser')
+        self.assertEqual(f'{self.post.body}', 'Nice body content')
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Nice body content')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_post_detail_view(self):
+        response = self.client.get('/post/1/')
+        no_response = self.client.get('/post/100000/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertTemplateUsed(response, 'post_detail.html')
